5e10035278e52c9283a07973f05d89eb3566e3a3
Carmen Romano <my-9@hotmail.it>
2024-06-18 13:34:30 +0200

    struttura base

---
+++ spring-components/src/main/java/carmenromano/spring_components/SpringComponentsApplication.java
@@ -0,0 +1,13 @@
+package carmenromano.spring_components;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringComponentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComponentsApplication.class, args);
+	}
+
+}
---
+++ spring-components/src/test/java/carmenromano/spring_components/SpringComponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package carmenromano.spring_components;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringComponentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
