e29e08787a45d7612c55b07da78e94d9e561d0a8
Carmen Romano <my-9@hotmail.it>
2024-06-18 13:44:58 +0200

    importato progetto d1

--- src/main/java/carmenromano/spring_components/SpringComponentsApplication.java
+++ src/main/java/carmenromano/spring_components/SpringComponentsApplication.java
@@ -1,13 +1,24 @@
-package carmenromano.spring_components;
 
+package carmenromano.spring_components;
+import carmenromano.spring_components.entities.Menu;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class SpringComponentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringComponentsApplication.class, args);
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringComponentsApplication.class);
+
+		Menu m = (Menu) ctx.getBean("menu");
+
+		m.printMenu();
+		ctx.close();
 	}
 
 }
